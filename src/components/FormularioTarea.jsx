519bbc099f2c4720d6255178565d3b9af118ca0b
Camila Fuentes Maldonado <camilafuentesmaldonado@gmail.com>
2022-10-20 12:17:39 -0300

    almacenamos las tareas en el localstorage

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,9 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect} from 'react';
 import ListaTarea from './ListaTarea';
 import { Form,Button } from 'react-bootstrap';
 const FormularioTarea = () => {
+  //busdco los datos del localstorage
+  //hace que busque los datos del local storage y si no hubiese nada lo manda al arreglo vacío
+  const tareasLocalStorage = JSON.parse(localStorage.getItem('arregloTareaKey')) || [];
     const [tarea, setTarea] = useState('');
-    const [arregloTarea, setarregloTarea] = useState([]);
+    const [arregloTarea, setarregloTarea] = useState(tareasLocalStorage);
+
+    //ciclo de vida
+useEffect(()=>{
+  //escrito asi funcona en el montaje y actualizacion del componente
+  //seria escrito useEffect{()=>{bla}}
+  //ejecuta logica pero no actualizacion si pongo, solo funcona en el montaje
+  //useEffect{()=>{blabla,[]}}
+  // si escribo useeffect con [arreglotara ]es para que fincione en montje y acrualizacion solo del arreglo 
+
+  //guardar en localstorage
+ 
+  localStorage.setItem('arregloTareaKey', JSON.stringify(arregloTarea))
+  console.log('hola')
+},[arregloTarea])
 
     const handleSubmit = (e)=>{
  e.preventDefault();
